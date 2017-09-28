c6f553399d199ace4835353a89e0078a414a2042
henrikschwarz <henrik.hasmark@gmail.com>
2017-09-28 12:36:27 +0200

    Henriks sierpinsky triangle

--- sierpinsky triangle.py
+++ sierpinsky triangle.py
@@ -4,12 +4,11 @@ import turtle
 bob = turtle.Turtle()
 
 def triangle(x, d): #x is size of triangle, d is depth
-colorlist = ["red", "blue", "green", "yellow"]
-    if len(colorlist) < d:
-        colorlist = colorlist*(d//4+1)
     for i in range(3):
         bob.fd(x)
         bob.lt(120)
         if d > 1:
             triangle(x/2, d-1)
-triangle(100,3)
+bob.speed(0)
+triangle(100,8)
+turtle.exitonclick()
