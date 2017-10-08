8f8311eb587d3f7de3cd3c8aa01decafc77c2cc9
longdahl <mikkel.bruunjensen@gmail.com>
2017-10-08 21:28:44 +0200

    Added warnings to fern_run for input values outside the suggested range

--- Fern Time.py
+++ Fern Time.py
@@ -14,6 +14,11 @@ def fern(t,x,d,angle,color,colorstep,s):
         t.lt(90)
     t.bk(x)
 def run_fern(x,d,angle,s=0.3): #d is depth
+    if s*50 > x:
+        print("WARNING: Your input value of s is relatively high compared to x this sacrifices alot of details"
+              " for very little speed. Try values of s around x/100 or lower.")
+    if angle < 75 or angle > 105:
+        print("WARNING: to create more fern like output, try values of angle around 80-100")
     t = turtle.Turtle()
     """ Add speed as an optional last argument to increase drawing speed with graph details as tradeoff (set to 0.3)"""
     turtle.tracer(250,100)
@@ -22,4 +27,4 @@ def run_fern(x,d,angle,s=0.3): #d is depth
     fern(t,x,d,angle,color,colorstep,s)
     turtle.update()
     turtle.exitonclick()
-run_fern(100,12,98)
+run_fern(100,12,106)
