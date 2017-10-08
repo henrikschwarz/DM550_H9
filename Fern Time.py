168fc0b6f08ec31cab246f59146bce599d633a6f
longdahl <mikkel.bruunjensen@gmail.com>
2017-10-08 19:22:00 +0200

    moved the turtle initialization inside the run function

--- Fern Time.py
+++ Fern Time.py
@@ -1,27 +1,26 @@
 import turtle
-t = turtle.Turtle()
-def fern(x,d,d2,angle,color,colorstep,s):
+def fern(t,x,d,angle,color,colorstep,s):
     t.color([0,color,0])
     if x < s: # s is speed
         return
     t.fd(x)
-    if d > 0 and d2 > 0:
+    if d > 0:
         t.lt(angle)
-        fern(x/2.8,d-1,d2-1,angle,color,colorstep,s)
+        fern(t,x/2.8,d-1,angle,color,colorstep,s)
         t.rt(90)
-        fern(x/1.3,d,d2-1,angle,color+colorstep,colorstep,s)
+        fern(t,x/1.3,d-1,angle,color+colorstep,colorstep,s)
         t.rt(angle)
-        fern(x/2.8,d-1,d2-1,angle,color,colorstep,s)
+        fern(t,x/2.8,d-1,angle,color,colorstep,s)
         t.lt(90)
     t.bk(x)
-def test_fern(x,d,angle,speed2=0.3): #d is depth
+def run_fern(x,d,angle,speed2=0.3): #d is depth
+    t = turtle.Turtle()
     """ Add speed as an optional last argument to increase drawing speed with graph details as tradeoff (set to 0.3)"""
     turtle.tracer(250,100)
     color = 0.3
     s = speed2
     colorstep = (1 - color) / d
-    d2 = d
-    fern(x,d,d2,angle,color,colorstep,s)
+    fern(t,x,d,angle,color,colorstep,s)
     turtle.update()
     turtle.exitonclick()
-test_fern(100,12,98)
+run_fern(100,12,98,0.3)
