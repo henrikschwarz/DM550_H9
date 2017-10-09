c8bded8974333b931d0bb67cb0430d0000128e79
longdahl <mikkel.bruunjensen@gmail.com>
2017-10-09 09:22:05 +0200

    created a warnings function instead

--- Fern Time.py
+++ Fern Time.py
@@ -13,12 +13,19 @@ def fern(t,x,d,angle,color,colorstep,s):
         fern(t,x/2.8,d-1,angle,color,colorstep,s)
         t.lt(90)
     t.bk(x)
-def run_fern(x,d,angle,s=0.3): #d is depth
-    if s*50 > x:
-        print("WARNING: Your input value of s is relatively high compared to x this sacrifices alot of details"
+def warnings(x,d,angle,s):
+    if s * 50 > x:
+        print("WARNING: Your input value of s is relatively high compared to x this sacrifices a lot of details"
               " for very little speed. Try values of s around x/100 or lower.")
     if angle < 75 or angle > 105:
-        print("WARNING: to create more fern like output, try values of angle around 80-100")
+        print("WARNING: large values of d has practically no effect on the branches visible to the eye."
+              + '\n' + "Furthermore it causes issues with the color parameter, and could cause slow drawing speeds when paired with low values of s")
+    if d > 20:
+        print("WARNING: large values of d has practically no effect on the branches visible to the eye."
+              + '\n' + "Furthermore it causes issues with the color parameter, and could cause slow drawing speeds when paired with low values of s")
+    else:
+        return
+def run_fern(x,d,angle,s=0.3): #d is depth
     t = turtle.Turtle()
     """ Add speed as an optional last argument to increase drawing speed with graph details as tradeoff (set to 0.3)"""
     turtle.tracer(250,100)
@@ -27,4 +34,8 @@ def run_fern(x,d,angle,s=0.3): #d is depth
     fern(t,x,d,angle,color,colorstep,s)
     turtle.update()
     turtle.exitonclick()
-run_fern(100,12,106)
+    warnings(x,d,angle,s)
+run_fern(100,21,98,3)
+
+
+
